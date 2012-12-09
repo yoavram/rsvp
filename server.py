ae018f0998503e05213d1ffec67963d12d325c87
Yoav Ram <yoavram+github@gmail.com>
2012-12-09 11:11:26 +0200

    index now allows creation of events, events show on their own pages

--- server.py
+++ server.py
@@ -2,16 +2,29 @@
 from flask import Flask, request, render_template
 from flask.ext.pymongo import PyMongo, ASCENDING 
 import os
-from os.path import exists
 from datetime import datetime
+import urllib
+import urlparse 
+
+DATE_FORMAT = "%d %B, %Y"
 
 def db_name_from_uri(full_uri):
 	ind = full_uri[::-1].find('/')
 	return full_uri[-ind:]
 
+
+def datetime_from_string(string):
+	return datetime.strptime(string, DATE_FORMAT)
+
+
+def string_from_datetime(datetime_obj):
+	return datetime_obj.strftime(DATE_FORMAT)
+
+
 # add environment variables using 'heroku config:add VARIABLE_NAME=variable_name'
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 MONGO_URI = os.environ.get('MONGOLAB_URI')
+WEBSITE_URL = 'http://localhost:5000/'
 PASSWORD = u'yoavram'
 
 app = Flask(__name__)
@@ -27,48 +40,73 @@ if mongo:
 else:
 	raise Exception("No Mongo Connection")
 
+app.jinja_env.filters['format_date'] = string_from_datetime
 
-def add_name(name):
+def process_form(form):
+	date = form['date']
+	if date:
+		form['date'] = datetime_from_string(date)
+	form['url'] = urlparse.urljoin(app.config["WEBSITE_URL"], 'event/' + urllib.quote_plus(form['event']))
+	return form
+
+
+def get_event(event):
+	return mongo.db.events.find_one({"event":event})
+
+def add_name(name, event):
 	time = datetime.now()
 	d = {'time':time, 'name':name}
-	oid = mongo.db.general.insert(d)
+	oid = mongo.db[event].insert(d)
 	print "Added object to database:", oid
 
 
-def get_names():
-	cur = mongo.db.general.find(sort=[('time', ASCENDING)])
+def get_names(event):
+	cur = mongo.db[event].find(sort=[('time', ASCENDING)])
 	names = [d['name'].strip() for d in cur]
 	names = [n for n in names if len(n) > 0]
 	return names
 
 
-def save_file(file_text):
-	mongo.db.general.drop()
-	for n in file_text.split('\n'):
+def save_file(editor, event):
+	mongo.db[event].drop()
+	for n in editor.split('\n'):
 		n = n.strip()
 		add_name(n)
 
 @app.route("/",  methods=['GET', 'POST'])
-def index():
-	names = get_names()
+def create():
 	if request.method == 'GET':
-		return render_template("index.html", total=len(names))
+		return render_template("create.html")
+	else:
+		form = process_form(request.form.to_dict())
+		oid = mongo.db.events.insert(form)
+		print "Added object to database:", oid
+		return render_template("create.html", form=form)
+
+
+@app.route("/event/<string:event>",  methods=['GET', 'POST'])
+def event(event):
+	event = urllib.unquote_plus(event)
+	event_details = get_event(event)
+	names = get_names(event)
+	if request.method == 'GET':
+		return render_template("index.html", event=event, total=len(names))
 	else:
 		if 'name' in request.form:
 			name = request.form['name']
 			if name in [u'John Doe', u'', u'', u' ', u' ', u'Your Name...'] :
 				return render_template("index.html", total=len(names), error=u"Please fill in your name")
-			elif name == PASSWORD:
-				file_text = "\n".join(names)
-				return render_template("index.html", file_text=file_text, total=len(names))
+			elif name == event_details['password']:
+				editor = "\n".join(names)
+				return render_template("editor.html", event=event, editor=editor, total=len(names))
 			else:
-				add_name(name)
-				return render_template("index.html", name=name, total=len(names))
-		elif 'file_text' in request.form:
-			file_text = request.form['file_text']
-			save_file(file_text)
+				add_name(name, event)
+				return render_template("index.html", event=event, name=name, total=len(names))
+		elif 'editor' in request.form:
+			editor = request.form['editor']
+			save_file(editor)
 			file_text = "\n".join(names)
-			return render_template("index.html", file_text=file_text, total=len(names))
+			return render_template("editor.html", event=event, editor=editor, total=len(names))
 
 
 if __name__ == '__main__':
